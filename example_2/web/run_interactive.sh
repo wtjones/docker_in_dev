cd83f794560905e73112c3c93bebe25087073aa7
William Jones <wtravisjones@gmail.com>
2016-12-24 17:07:50 -0600

    redis cleanup

--- example_2/web/run_interactive.sh
+++ example_2/web/run_interactive.sh
@@ -2,5 +2,4 @@
 
 docker run -it -p 80:80 -v "$(pwd)":/code \
     --name flaskdemo \
-    --restart=always \
     wtjones/flaskdemo flask run --host=0.0.0.0 --port 80
--- example_3/web/app.py
+++ example_3/web/app.py
@@ -8,13 +8,17 @@ r = redis.Redis(host=os.environ['FLASKDEMOREDIS_PORT_6379_TCP_ADDR'])
 
 @app.route("/")
 def index():
-    users = r.smembers('users')
-    return render_template('index.html', message="Hello World!", users=users)
+    users = []
+    
+    # redis provides byte[], so convert to string
+    for user in r.smembers('users'):
+        users.append(user.decode('UTF-8'))
+
+    return render_template('index.html', message="Please register!", users=users)
 
 @app.route('/register', methods=['POST'])
 def register():
     user = request.form['user']
-
-    users.append(user)
-    print (user)
+    r.sadd('users', user)
+    print ('adding user: {0}'.format(user))
     return redirect(url_for('index'))
