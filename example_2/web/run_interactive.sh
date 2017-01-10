b89f691ddb4785be7a116c702390709e8cdd5a84
William Jones <wtravisjones@gmail.com>
2017-01-09 19:30:14 -0600

    Fix demo scripts

--- example_2/web/run_interactive.sh
+++ example_2/web/run_interactive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -it -p 80:80 -v "$(pwd)":/code \
+docker run --rm -it -p 80:80 -v "$(pwd)":/code \
     --name flaskdemo \
     wtjones/flaskdemo flask run --host=0.0.0.0 --port 80
--- example_3/web/run_detached.sh
+++ example_3/web/run_detached.sh
@@ -2,5 +2,5 @@
 
 docker run -d -p 80:80 -v "$(pwd)":/code \
     --name flaskdemo \
-    --restart=always \
-    wtjones/flaskdemo python3 app.py
+    --link flaskdemoredis \
+    wtjones/flaskdemo flask run --host=0.0.0.0 --port 80
